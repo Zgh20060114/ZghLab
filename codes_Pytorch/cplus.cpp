661f9269a8a80c8886bad858e919f802681d3945
zgh20060114 <2987408354@qq.com>
2026-07-21 13:32:04 +0800

    cpp_wd

--- codes_Pytorch/cplus.cpp
+++ codes_Pytorch/cplus.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+#include <memory>
 #include <vector>
+
+class Point {
+public:
+  // Point() = default;
+  Point(int x = 0, int y = 0) : _x(x), _y(y) {
+    std::cout << "调用构造函数" << std::endl;
+  }
+  Point(const Point &rhs) { std::cout << "调用拷贝构造函数" << std::endl; }
+  Point &operator=(const Point &rhs) {
+    std::cout << "调用拷贝赋值函数" << std::endl;
+    return *this;
+  }
+  Point(Point &&rhs) { std::cout << "调用移动构造函数" << std::endl; }
+  Point &operator=( Point &&rhs) {
+    std::cout << "调用移动赋值函数" << std::endl;
+    return *this;
+  }
+
+private:
+  const int _x = 0;
+  const int _y = 0;
+};
 
 int main(int argc, char *argv[]) {
   // auto vec = std::vector<int>{1, 2, 3};
   // std::cout << vec << std::endl;
-  std::cout << sizeof(int) << std::endl;
+  // auto p1_ptr = std::make_unique<Point>(1, 2);
+  // auto p1_ptr = std::make_unique<Point>();
+  // auto p = Point{1, 2};
+  // auto p1_vec = std::vector<Point>{p};
+  std::vector<Point> p2_vec{Point{3, 4}};
+  // std::cout << sizeof(int) << std::endl;
   return 0;
 }
-
-class Point {
-public:
-  Point(int x, int y) : _x(x), _y(y) {}
-  // Point(int x, int y){
-  //   _x = x;
-  //   _y = y;
-  // }
-
-private:
-  const int _x;
-  const int _y;
-};
